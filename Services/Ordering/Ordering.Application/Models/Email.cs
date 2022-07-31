a0e7a51deaf12425c0c8066586a4e805f00af96c
Gabriel Cesario <gcesario203@gmail.com>
2022-07-31 13:40:53 -0300

    feat: dependencias e estruturação de serviços

---
+++ Services/Ordering/Ordering.Application/Models/Email.cs
@@ -0,0 +1,9 @@
+namespace Ordering.Application.Models
+{
+    public class Email
+    {
+        public string To { get; set; }
+        public string From { get; set; }
+        public string Body { get; set; }
+    }
+}

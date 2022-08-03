7966326d176a2c5397a6bf0045f587f7da24752b
Gabriel Cesario <gcesario203@gmail.com>
2022-08-03 08:15:43 -0300

    fix: coisas erradas não certas

--- Services/Ordering/Ordering.Application/Features/Orders/Queries/OrderViewModel.cs
+++ Services/Ordering/Ordering.Application/Features/Orders/Queries/OrderViewModel.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace Ordering.Application.Features.Orders.Queries.GetOrdersList
 {
     public class OrderViewModel

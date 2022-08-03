cde0a7f71b8ae76090619ae3accbad7263bfbdef
Gabriel Cesario <gcesario203@gmail.com>
2022-08-03 16:39:00 -0300

    progess:serviços de injeção da camada de aplicação

---
+++ Services/Ordering/Ordering.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,16 @@
+
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ordering.Application
+{
+    public static class ApplicationServiceRegistration
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+            return services;
+        }
+    }
+}

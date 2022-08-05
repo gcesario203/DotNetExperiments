a586b40531996b633ec8a002da601185edfc4c66
Gabriel Cesario <gcesario203@gmail.com>
2022-08-04 22:03:25 -0300

    feat: Repositorios + Envio de emails com SendGrid

---
+++ Services/Ordering/Ordering.Infrastructure/Repositories/OrderRepository.cs
@@ -0,0 +1,14 @@
+using Ordering.Application.Contracts.Persistence;
+using Ordering.Domain.Entities;
+using Ordering.Infrastructure.Persistence;
+
+namespace Ordering.Infrastructure.Repositories
+{
+    public class OrderRepository : RepositoryBase<Order>, IOrderRepository
+    {
+        public OrderRepository(OrderContext dbContext) : base(dbContext)
+        {
+        }
+        public async Task<IEnumerable<Order>> GetOrdersByUserName(string userName) => await GetAsync(x => x.UserName == userName);
+    }
+}
